85b8a3ca32a6485e2ac78d3757e1cfa2470a3f0b
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2024-07-09 09:38:21 -0500

    Add Demo of Inherited Scene

---
+++ Godot/aaDevelopers/SamR/InheritedSceneDemo/Dog.cs
@@ -0,0 +1,57 @@
+﻿
+namespace RMC.Core.Templates
+{
+	/// <summary>
+	/// TODO: Add comments
+	/// </summary>
+	public partial class Dog : Animal
+	{
+		//  Events ----------------------------------------
+        
+		
+		//  Properties ------------------------------------
+        
+		
+		//  Fields ----------------------------------------
+        
+		
+		//  Initialization  -------------------------------
+		public Dog() : base ()
+		{
+			
+		}
+        
+		//  Godot Methods ---------------------------------
+		/// <summary>
+		/// Called when the node enters the scene tree for the first time.
+		/// </summary>
+		public override void _Ready()
+		{
+			base._Ready();
+		}
+
+        
+		/// <summary>
+		/// Called every frame. 'delta' is the elapsed time since the previous frame.
+		/// </summary>
+		public override void _Process(double delta)
+		{
+			base._Process(delta);
+		}
+		
+		
+		/// <summary>
+		/// Called when the node is about to leave the SceneTree
+		/// </summary>
+		public override void _ExitTree()  
+		{
+			base._ExitTree();
+		}
+
+		
+		//  Methods ---------------------------------------
+        
+		
+		//  Event Handlers --------------------------------
+	}
+}
