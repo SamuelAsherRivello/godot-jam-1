96a319c140be11f137cdf0426bc13235cd31b920
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2024-07-09 13:57:28 -0500

    Better starting point

--- Godot/Scripts/Runtime/RMC/Racing2D/Standard/Tracks/TrackStartingArea.cs
+++ Godot/Scripts/Runtime/RMC/Racing2D/Standard/Tracks/TrackStartingArea.cs
@@ -5,6 +5,9 @@ namespace RMC.Racing2D.Tracks
 {
     public partial class TrackStartingArea : Node3D
     {
+        [Export]
+        public Area3D Area3D;
+        
         [Export] 
         public Array<Node3D> StartingPoints;
     }
